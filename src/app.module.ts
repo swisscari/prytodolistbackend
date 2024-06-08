96fc87ec5f9fe6a3edfd07d9c0927d53b04989bb
swisscari <swisscartoryy@gmail.com>
2024-06-08 16:44:52 -0400

    update proj

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        return {
+          type: 'postgres',
+          url: process.env.DB_CONN,
+        }
+      }
+    })
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { config } from 'dotenv'
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+config()
+const swaggerConfig = new DocumentBuilder()
+  .setTitle('Customer City Swagger')
+  .setDescription('Customer City - Technical Assessment')
+  .setVersion('1.0')
+  .addTag('tasks')
+  .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+
+  app.useGlobalPipes(new ValidationPipe({
+    stopAtFirstError: true,
+  }))
+
+  SwaggerModule.setup('/', app, swaggerDoc);
+  await app.listen(process.env.PORT);
 }
+
 bootstrap();
